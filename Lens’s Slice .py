fd9fa313dc061f4c871acacc68d4a2761c484e21
topistalamaro <82517366+topistalamaro@users.noreply.github.com>
2021-07-10 11:08:42 -0700

    Add files via upload

---
+++ Lens’s Slice .py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Lens’s Slice 
+
+# In[2]:
+
+
+# Your code below:
+toppings = ["pepperoni","pineapple","cheese","sausage","olives","anchovies","mushrooms"]
+prices = [2,6,1,3,2,7,2] 
+num_two_dollar_slices = prices.count(2)
+print("")
+print(num_two_dollar_slices)
+
+num_pizzas = len(toppings)
+print("")
+print("We sell " + str(num_pizzas) + " different kinds of pizza!" )
+
+pizza_and_prices = [[2, "pepperoni"], [6, "pineapple"],[1,"cheese"], [3, "sausage"], [2, "olives"], [7, "anchovies"], [2,"mushrooms"]]
+print("")
+print(pizza_and_prices)
+
+pizza_and_prices.sort()
+
+cheapest_pizza = pizza_and_prices[0]
+priciest_pizza = pizza_and_prices[-1]
+pizza_and_prices.pop(-2)
+pizza_and_prices.insert(4, [2.5, "peppers"])
+
+print("")
+print("Sorted pizzas")
+print(pizza_and_prices)
+
+three_cheapest = pizza_and_prices[:3]
+print('')
+print("Three cheapest pizzas")
+print(three_cheapest)
+
+
+# In[ ]:
+
+
+
+
